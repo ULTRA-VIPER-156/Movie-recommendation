1a88ebb67050efce3662c81635d6422f9635932a
Jonathan <netsijonathan@gmail.com>
2025-06-30 23:39:54 +0200

    Add files via upload
    update to components added a main recommendations

---
+++ Componets/MovieRec.tsx
@@ -0,0 +1,221 @@
+import './MovieRec.css';
+
+function MovieRec() 
+{ 
+    return (
+     <div>
+        <br /><br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        
+        <h2 className="MovieRec-title">
+            Recommendend Movies 
+        </h2>
+        <div className="Main">
+            <br />
+            <ul>
+                <li>
+                    <div className="card1">
+                        <h2 className="Movie-title">
+                            Minecraft Movie
+                        </h2>
+                        <img className="MovieImg" src="https://m.media-amazon.com/images/I/71nXe3u4XCL._AC_UF894,1000_QL80_.jpg" alt="Minecraft Movie" />
+                        <a className='ViewMore' href="https://www.themoviedb.org/movie/123456-minecraft-movie">
+                            View More
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div className="card1">
+                        <h2 className="Movie-title">
+                            The Shawshank ...
+                        </h2>
+                        <img className="MovieImg" src="https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg" alt="The Shawshank Redemption" />
+                        <a className='ViewMore' href="https://www.themoviedb.org/movie/278-the-shawshank-redemption">
+                            View More
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div className="card1">
+                        <h2 className="Movie-title">
+                            The Godfather
+                        </h2>
+                        <img className="MovieImg" src="https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg" alt="The Godfather" />
+                        <a className='ViewMore' href="https://www.themoviedb.org/movie/238-the-godfather">
+                            View More
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div className="card1">
+                        <h2 className="Movie-title">
+                            The Dark Knight
+                        </h2>
+                        <img className="MovieImg" src="https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg" alt="The Dark Knight" />
+                        <a className='ViewMore' href="https://www.themoviedb.org/movie/155-the-dark-knight">
+                            View More
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div className="card1">
+                        <h2 className="Movie-title">
+                            Inception
+                        </h2>
+                        <img className="MovieImg" src="https://image.tmdb.org/t/p/w500/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg" alt="Inception" />
+                        <a className='ViewMore' href="https://www.themoviedb.org/movie/27205-inception">
+                            View More
+                        </a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <br /><br /><br /><br />
+        <div>
+            <h2 className="MovieRec-title">
+                Anime (spring 2024)
+            </h2>
+            <div className="Main">
+                <ul>
+                    <li>
+                        <div className="card1">
+                            <h2 className="Movie-title">
+                                Jujutsu Kaisen
+                            </h2>
+                            <img className="MovieImg" src="https://image.tmdb.org/t/p/w500/3pTwMUEavTzVOh6yLN0aEwR7uSy.jpg" alt="Jujutsu Kaisen 0 (Movie)" />
+                            <a className='ViewMore' href="https://www.themoviedb.org/movie/123456-jujutsu-kaisen">
+                                View More
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="card1">
+                            <h2 className="Movie-title">
+                                Demon Slayer
+                            </h2>
+                            <img className="MovieImg" src="https://image.tmdb.org/t/p/w500/h8Rb9gBr48ODIwYUttZNYeMWeUU.jpg" alt="Demon Slayer: Mugen Train" />
+                            <a className='ViewMore' href="https://www.themoviedb.org/movie/123456-demon-slayer">
+                                View More
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="card1">
+                            <h2 className="Movie-title">
+                                Attack on Titan
+                            </h2>
+                            <img className="MovieImg" src="https://image.tmdb.org/t/p/w500/9nBVAX9T2BLPzPYw0xXsm5U5O4x.jpg" alt="Attack on Titan Final Season" />
+                            <a className='ViewMore' href="https://www.themoviedb.org/movie/123456-attack-on-titan">
+                                View More
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="card1">
+                            <h2 className="Movie-title">
+                                My Hero Academia
+                            </h2>
+                            <img className="MovieImg" src="https://image.tmdb.org/t/p/w500/a5uPbVBqSgWc8jYh5Y8kHoX35eF.jpg" alt="My Hero Academia: Heroes Rising" />
+                            <a className='ViewMore' href="https://www.themoviedb.org/movie/123456-my-hero-academia">
+                                View More
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="card1">
+                            <h2 className="Movie-title">
+                                One Piece
+                            </h2>
+                            <img className="MovieImg" src="https://image.tmdb.org/t/p/w500/3Rfvhy1Nl6sSGJwyjb0QiZzZYlB.jpg" alt="One Piece: Red" />
+                            <a className='ViewMore' href="https://www.themoviedb.org/movie/123456-one-piece">
+                                View More
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <br /><br /><br /><br />
+        <div>
+            <h2 className="MovieRec-title">
+                Trending Movies
+            </h2>
+            <div className="Main">
+                <ul>
+                    <li>
+                        <div className="card1">
+                            <h2 className="Movie-title">
+                                The Super Mario Bros. Movie
+                            </h2>
+                            <img className="MovieImg" src="https://image.tmdb.org/t/p/w500/qNBAXBIQlnOThrVvA6mA2B5ggV6.jpg" alt="The Super Mario Bros. Movie" />
+                            <a className='ViewMore' href="https://www.themoviedb.org/movie/123456-the-super-mario-bros-movie">
+                                View More
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="card1">
+                            <h2 className="Movie-title">
+                                Final Destination 6
+                            </h2>
+                            <img className="MovieImg" src="https://image.tmdb.org/t/p/w500/pTwTH1hG3ueBM9EkYTwC8FkUxJZ.jpg" alt="Final Destination 5" />
+                            <a className='ViewMore' href="https://www.themoviedb.org/movie/123456-final-destination-6">
+                                View More
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="card1">
+                            <h2 className="Movie-title">
+                                Fast X
+                            </h2>
+                            <img className="MovieImg" src="https://image.tmdb.org/t/p/w500/fiVW06jE7z9YnO4trhaMEdclSiC.jpg" alt="Fast X" />
+                            <a className='ViewMore' href="https://www.themoviedb.org/movie/123456-fast-x">
+                                View More
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="card1">
+                            <h2 className="Movie-title">
+                                Guardians of the Galaxy Vol. 3
+                            </h2>
+                            <img className="MovieImg" src="https://image.tmdb.org/t/p/w500/r2J02Z2OpNTctfOSN1Ydgii51I3.jpg" alt="Guardians of the Galaxy Vol. 3" />
+                            <a className='ViewMore' href="https://www.themoviedb.org/movie/123456-guardians-of-the-galaxy-vol-3">
+                                View More
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="card1">
+                            <h2 className="Movie-title">
+                                28 Days Later 
+                            </h2>
+                            <img className="MovieImg" src="https://image.tmdb.org/t/p/w500/tH0zVbZpM5y8AAgF46XnYE7vCzg.jpg" alt="28 Days Later" />
+                            <a className='ViewMore' href="https://www.themoviedb.org/movie/123456-28-days-later">
+                                View More
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default MovieRec;
