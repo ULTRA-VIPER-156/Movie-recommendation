2f6ddfc44d826f03dc98ee11148562ab65ade0b4
Jonathan <netsijonathan@gmail.com>
2025-07-01 18:49:52 +0200

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,22 @@
+
+import MovieRec from "./Componets/MovieRec";
+import './App.css';
+import MoviePlayer from "./Componets/MoviePlayer";
+
+
+function App() 
+{
+
+  return (
+    
+    <div className="App">
+      <MoviePlayer />
+      <MovieRec />
+     
+      </div>
+
+);
+}
+
+export default App;
+  
